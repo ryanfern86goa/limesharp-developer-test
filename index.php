d45826fbbb2a4aa9635886dd36a7e33850017e0c
ryanfern86goa <ryanfern86goa@googlemail.com>
2016-08-04 20:01:11 +0100

    task-2 reove vowel

--- index.php
+++ index.php
@@ -12,4 +12,16 @@ for ($i=0; $i<3; $i++) {
     }
 }
 
+/**
+ * task 2, remove vowel
+ */
+
+// convert to lower case
+$word = strtolower("liMeSHArp DeveLoper TEST");
+
+echo '<br/><br/>';
+
+// remove vowel from string, upper case first character of word
+echo ucwords(preg_replace("/([aeiou]+)/i", "", $word));
+
 ?>
